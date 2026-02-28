b025a5d148b112cdd7ad83c2ad1ab017650aae17
Yuriy Egorov <viohneq@gmail.com>
2026-02-28 14:06:10 +0000

    refactor(domain): update engine type references to Audio.AVMetronomeEngine, Audio.AVPitchDetector, Audio.TonePlayerEngine

--- Nocturne/Nocturne/Domain/Metronome/Feature/MetronomeEffects.swift
+++ Nocturne/Nocturne/Domain/Metronome/Feature/MetronomeEffects.swift
@@ -87,7 +87,7 @@ extension Metronome.Effects {
 
 extension Metronome.Effects {
     static func live(
-        engine: AVAudioMetronomeEngine,
+        engine: Audio.AVMetronomeEngine,
         settings: UserDefaultsSettingsStore
     ) -> Metronome.Effects {
         Metronome.Effects(
